f01f9da2ec992091e245f55a807ae68a43816391
Vijay-2021 <42916674+Vijay-2021@users.noreply.github.com>
2021-07-31 16:36:55 -0400

    more firebase tests

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-// TODO: Replace with your project's config object. You can find this
-// by navigating to your project's console overview page
-// (https://console.firebase.google.com/project/your-project-id/overview)
-// and clicking "Add Firebase to your web app"
-var config = {
-    apiKey: "AIzaSyDIguvH3NqDwhuNpO6CESQdv09t9qWQzlk",
-    authDomain: "liarspoker-b0f12.firebaseapp.com",
-    projectId: "liarspoker-b0f12",
-    storageBucket: "liarspoker-b0f12.appspot.com",
-};
-
-// Initialize your Firebase app
-firebase.initializeApp(config);
 
 // Reference to your entire Firebase database
-var myFirebase = firebase.database().ref();
+var myFirebase = firebase.database();
 
 // Get a reference to the recommendations object of your Firebase.
 // Note: this doesn't exist yet. But when we write to our Firebase using
