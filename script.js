98da10c50ae22778bda12838ce52d5abd3e58144
Vijay-2021 <42916674+Vijay-2021@users.noreply.github.com>
2021-07-31 16:46:44 -0400

    got firebase push to database to work

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ var myFirebase = firebase.database();
 // Get a reference to the recommendations object of your Firebase.
 // Note: this doesn't exist yet. But when we write to our Firebase using
 // this reference, it will create this object for us!
-var boardsTest = myFirebase.child("boards_test");
+var boardsTest = myFirebase.ref().child("boards_test");
 
 // Push our first recommendation to the end of the list and assign it a
 // unique ID automatically.
-recommendations.push({
+boardsTest.push({
     "type":"test",
 });
