623457c1656c4701ae54d99643abdae0c2ca0222
MichaelYesuyu <michaelyesuyu@gmail.com>
2021-08-02 15:28:38 -0400

    testing generation on button click

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,8 @@ import Deck from './deck.js'
 const deck = new Deck()
 deck.shuffle()
 console.log(deck.cards)
+
+function generate(){
+    document.getElementById ('generateCard').innerHTML += 
+        `<img src="assets/10C.jpg" class="img-fluid">`
+}
