25402ae98244420b6939383cb338ab2cae1b1d84
MichaelYesuyu <michaelyesuyu@gmail.com>
2021-08-05 22:31:44 -0400

    sign up and login

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,6 @@ import Deck from './deck.js'
 const deck = new Deck()
 deck.shuffle()
 console.log(deck.cards)
-
-function generate(){
+$(document).ready(function(){
     
-    document.getElementById('generateCard').innerHTML += 
-        
-    `<img src="assets/10C.jpg" class="img-fluid">`
-}
+ }); 
