0b6082a8c286f5cd6e07edf393289fbcd0c1d6f9
MichaelYesuyu <michaelyesuyu@gmail.com>
2021-12-26 10:45:14 +0800

    made some progress to game round

--- basicGame.py
+++ basicGame.py
@@ -43,7 +43,7 @@ class Deck:
             card.printCard()
 
     def printDrawnCards(self):
-        testDeck = Deck.drawCards(self,5)
+        testDeck = Deck.drawCards(self,30)
         for card in testDeck:
             card.printCard()
 
@@ -85,12 +85,44 @@ def CheckRoyalFlush(cardPool, suit):
 
 #end of check cards
 
-deck = Deck()
-deck.buildDeck()
-deck.shuffleDeck()
+#supports up to 5 players, default is 2
+def gameRound(p1,p2,p3=None,p4=None,p5=None):
+    #creates a fresh shuffled deck
+    deck = Deck()
+    deck.buildDeck()
+    deck.shuffleDeck()
+
+    #draws cards for each player
+    player1Cards = deck.drawCards(p1.numCards)
+    p1.cardList = player1Cards
+    player2Cards = deck.drawCards(p2.numCards)
+    p2.cardList = player2Cards
+    if p3 is not None:
+        player3Cards = deck.drawCards(p3.numCards)
+        p3.cardList = player3Cards
+    if p4 is not None:
+        player4Cards = deck.drawCards(p4.numCards)
+        p4.cardList = player4Cards
+    if p5 is not None:
+        player5Cards = deck.drawCards(p5.numCards)
+        p5.cardList = player5Cards
+
+    #2 ways to end a round - call bluff or declare royal flush
+    callBluff = False
+    royalFlush = False
+
+    while not callBluff and not royalFlush:
+        return # TODO
+
+    
+
+
+#deck = Deck()
+#deck.buildDeck()
+#deck.shuffleDeck()
 #deck.printCard()
-deck.printDrawnCards()
+#deck.printDrawnCards()
     
 #game starts below
-print('Game starting......')
+#print('Game starting......')
 
