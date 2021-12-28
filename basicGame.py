cf304a1ce39a201707e02d2444bd9e6a9d8db920
MichaelYesuyu <michaelyesuyu@gmail.com>
2021-12-28 19:03:37 +0800

    Made more progress to gameRound and started checks

--- basicGame.py
+++ basicGame.py
@@ -10,9 +10,6 @@ class Card:
     def printCard(self):
         print(str(self.value)+str(self.suit))
 
-#card = Card('d','6')
-#Card.printCard(card)
-
 class Deck:
     def __init__ (self):
         self.Cards = []
@@ -53,8 +50,11 @@ class Player:
         self.cardList = cardList
 
 #below are the check functions for seeing if the cards are there
-def CheckHigh(cardPoo, highCard):
-    return #TODO
+def CheckHigh(cardPool, highCard):
+    for card in cardPool:
+        if card.value == highCard:
+            return True
+    return False
 
 def CheckPair(cardPool, pairCard):
     return #TODO
@@ -85,7 +85,7 @@ def CheckRoyalFlush(cardPool, suit):
 
 #end of check cards
 
-#supports up to 5 players, default is 2
+#supports up to 5 players, default is 2, returns the player that lost the round
 def gameRound(p1,p2,p3=None,p4=None,p5=None):
     #creates a fresh shuffled deck
     deck = Deck()
@@ -93,36 +93,114 @@ def gameRound(p1,p2,p3=None,p4=None,p5=None):
     deck.shuffleDeck()
 
     #draws cards for each player
-    player1Cards = deck.drawCards(p1.numCards)
-    p1.cardList = player1Cards
-    player2Cards = deck.drawCards(p2.numCards)
-    p2.cardList = player2Cards
+    p1Cards = deck.drawCards(p1.numCards)
+    p1.cardList = p1Cards
+    p2Cards = deck.drawCards(p2.numCards)
+    p2.cardList = p2Cards
     if p3 is not None:
-        player3Cards = deck.drawCards(p3.numCards)
-        p3.cardList = player3Cards
+        p3Cards = deck.drawCards(p3.numCards)
+        p3.cardList = p3Cards
     if p4 is not None:
-        player4Cards = deck.drawCards(p4.numCards)
-        p4.cardList = player4Cards
+        p4Cards = deck.drawCards(p4.numCards)
+        p4.cardList = p4Cards
     if p5 is not None:
-        player5Cards = deck.drawCards(p5.numCards)
-        p5.cardList = player5Cards
+        p5Cards = deck.drawCards(p5.numCards)
+        p5.cardList = p5Cards
 
     #2 ways to end a round - call bluff or declare royal flush
     callBluff = False
     royalFlush = False
 
-    while not callBluff and not royalFlush:
-        return # TODO
+    while True:
+        store_move = []
+        for p in [p1,p2,p3,p4,p5]:
+            if p is not None:
+                print('Your move, choose:')
+                print('1 - Call Bluff')
+                print('2 - High card')
+                print('3 - Pair')
+                print('4 - 2 Pair')
+                print('5 - 3 of a kind')
+                print('6 - Straight')
+                print('7 - Flush')
+                print('8 - Full House')
+                print('9 - Four of a kind')
+                print('10 - Straight Flush')
+                print('11 - Royal Flush')
+                
+                #makes sure user enters a valid response
+                while True:
+                    choice = input('Enter your choice')
+                    if store_move != [] or choice != '1':
+                        if choice in ['1','2','3','4','5','6','7','8','9','10','11']:
+                            break
+                        else:
+                            print('Please enter a number from 1 to 11')
+                    else:
+                        print('You cannot call bluff on the first move!')
+                
+                #handles the next move based on user's response
+                if choice =='1':
+                    if store_move[1] == '2':
+                        if CheckHigh(p1Cards+p2Cards+p3Cards+p4Cards+p5Cards,store_move[2]):
+                            return p #If not bluff, p loses
+                        else:
+                            return store_move[0] #If bluff, the player that made the bluff loses
+                    if store_move[1] == '3':
+                        if CheckPair(p1Cards+p2Cards+p3Cards+p4Cards+p5Cards,store_move[2]):
+                            return p
+                        else:
+                            return store_move[0]
+                    if store_move[1] == '4':
+                        if CheckTwoPair(p1Cards+p2Cards+p3Cards+p4Cards+p5Cards,store_move[2],store_move[3]):
+                            return p
+                        else:
+                            return store_move[0]
+                    
 
-    
+                    #use store_move to call one of the check functions, then update player cardnum
+                    break
+                elif choice == '2':
+                    choice2 = input('Enter High card')
+                    store_move = [p,choice,choice2]
 
+                elif choice == '3':
+                    choice2 = input('Enter Pair card')
+                    store_move = [p,choice,choice2]
 
-#deck = Deck()
-#deck.buildDeck()
-#deck.shuffleDeck()
-#deck.printCard()
-#deck.printDrawnCards()
-    
-#game starts below
-#print('Game starting......')
+                elif choice == '4':
+                    choice2 = input('Enter 1st pair card')
+                    choice3 = input('Enter 2nd pair card')
+                    store_move = [p,choice,choice2,choice3]
 
+                elif choice == '5':
+                    choice2 = input('Enter Triple card')
+                    store_move = [p,choice,choice2]
+
+                elif choice == '6':
+                    choice2 = input('Enter highest card in straight')
+                    #this one may need to prevent entering 2, 3, and 4
+                    store_move = [p,choice,choice2]
+
+                elif choice == '7':
+                    choice2 = input('Enter flush suite')
+                    choice3 = input('Enter high card in flush')
+                    store_move = [p,choice,choice2,choice3]
+
+                elif choice == '8':
+                    choice2 = input('Enter triple card')
+                    choice3 = input('Enter pair card')
+                    store_move = [p,choice,choice2,choice3]
+
+                elif choice == '9':
+                    choice2 = input('Enter quadruple card')
+                    store_move = [p,choice,choice2]
+
+                elif choice == '10':
+                    choice2 = input('Enter high card in straight')
+                    choice3 = input('Enter suite of straight')
+                    store_move = [p,choice,choice2,choice3]
+
+                elif choice == '11':
+                    choice2 = input('Enter suite of royal flush')
+                    store_move = [p,choice,choice2]
